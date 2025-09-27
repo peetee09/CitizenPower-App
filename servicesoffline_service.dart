2d2d4533502f90f6bf048ad1c6a04be624416793
peetee09 <93839860+peetee09@users.noreply.github.com>
2025-09-27 11:35:32 +0200

    Update servicesoffline_service.dart

--- servicesoffline_service.dart
+++ servicesoffline_service.dart
@@ -1,17 +1,31 @@
 import 'package:hive/hive.dart';
 import 'package:citizenpower/models/report.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 class OfflineService {
-  final Box _offlineBox = Hive.box('offlineReports');
+  late final Box _offlineBox;
+
+  OfflineService() {
+    if (!kIsWeb) {
+      _offlineBox = Hive.box('offlineReports');
+    }
+  }
 
   Future<List<Report>> getOfflineReports() async {
+    if (kIsWeb) {
+      return []; // Offline storage not supported on web
+    }
     return _offlineBox.values
         .where((item) => item['synced'] == false)
         .map((item) => item['report'] as Report)
         .toList();
   }
 
-  Future<void> saveReportForOffline(Report report, List<File> mediaFiles) async {
+  Future<void> saveReportForOffline(Report report, List<dynamic> mediaFiles) async {
+    if (kIsWeb) {
+      // Web: Always try to sync immediately
+      return;
+    }
     await _offlineBox.put(report.id, {
       'report': report,
       'mediaFiles': mediaFiles,
@@ -20,6 +34,8 @@ class OfflineService {
   }
 
   Future<void> removeSyncedReport(String reportId) async {
-    await _offlineBox.delete(reportId);
+    if (!kIsWeb) {
+      await _offlineBox.delete(reportId);
+    }
   }
 }
