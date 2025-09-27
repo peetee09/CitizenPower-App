a6705a679aefa77c22f9714bf46f0816044495c0
peetee09 <93839860+peetee09@users.noreply.github.com>
2025-09-27 13:31:50 +0200

    Create firebase_config.dart

---
+++ lib/utils/firebase_config.dart
@@ -0,0 +1,11 @@
+class FirebaseConfig {
+  static const webConfig = {
+    "apiKey": "AIzaSyA7ULBfHKEW93SXyr8CHeA63WFPHqyspvw",
+    "authDomain": "citizenpower-6b647.firebaseapp.com",
+    "projectId": "citizenpower-6b647",
+    "storageBucket": "citizenpower-6b647.firebasestorage.app",
+    "messagingSenderId": "50474846301",
+    "appId": "1:50474846301:web:4942dde8c9a32ddc77e5b4",
+    "measurementId": "G-4ZFYKY7291"
+  };
+}
